35d9bcc5fd3de9b41a8e078b103d847480813b36
lenhattri <lenhattri7@gmail.com>
2024-02-26 08:11:23 +0700

    delete dataset content

--- dataset/check_have_subtitle.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-from bs4 import ResultSet
-
-def checkHaveSubtitle(contents: ResultSet) -> bool:
-    for content in contents:
-        if content.findChild("strong") is not None:
-            return True
-    return False
